72605bf11a62007f0592981e99c2f3edb69e5d2d
Antoine James Tournepiche <antoinejt.serveur@gmail.com>
2025-08-28 01:54:15 +0200

    refactor: set configuration properties scan folder

--- src/main/java/fr/redstom/asynclevelling/AsyncLevellingApplication.java
+++ src/main/java/fr/redstom/asynclevelling/AsyncLevellingApplication.java
@@ -2,12 +2,14 @@ package fr.redstom.asynclevelling;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableJpaAuditing
 @EnableScheduling
+@ConfigurationPropertiesScan(basePackages = "fr.redstom.asynclevelling.config")
 public class AsyncLevellingApplication {
 
     public static void main(String[] args) {
