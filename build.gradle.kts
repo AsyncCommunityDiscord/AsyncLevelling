4a7f12f49d83c08d0f28c47e94f36bc27d6e0008
Antoine James Tournepiche <antoinejt.serveur@gmail.com>
2025-08-28 02:53:20 +0200

    refactor: cleanup build file

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,14 +1,12 @@
 plugins {
     java
-
     id("com.diffplug.spotless") version "7.2.1"
-
     id("org.springframework.boot") version "3.5.5"
     id("io.spring.dependency-management") version "1.1.7"
 }
 
 group = "fr.redstom"
-version = "0.0.1-SNAPSHOT"
+version = "1.0.0"
 
 java {
     toolchain {
@@ -28,14 +26,13 @@ repositories {
 
 dependencies {
     implementation("com.fasterxml.jackson.core:jackson-databind:2.19.2")
+    implementation("net.dv8tion:JDA:5.6.1")
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     implementation("org.springframework.boot:spring-boot-starter-jdbc")
 
     compileOnly("org.projectlombok:lombok")
     runtimeOnly("org.postgresql:postgresql")
     annotationProcessor("org.projectlombok:lombok")
-
-    implementation("net.dv8tion:JDA:5.6.1")
 }
 
 tasks.withType<Test> {
